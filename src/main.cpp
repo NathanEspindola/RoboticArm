aead3b6d9f86e53377d6ebbbf5bd2cd7c4b95386
NathanEspindola <espindolathan@gmail.com>
2023-05-08 23:56:51 -0300

    Removí data e hora do display

--- src/main.cpp
+++ src/main.cpp
@@ -54,8 +54,6 @@
 // SVP
 
 //#include <Arduino.h>
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <ESP32Servo.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
@@ -63,56 +61,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-// WiFI & MQTT ================================================================
-/*
-const char* ssid = "PUG";
-const char* password = "lovemy2pugs";
-
-const char* mqtt_server = "192.168.2.15";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
-
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
-void wifiSetup(void)
-{
-
-}*/
-
 // DISPLAY ================================================================
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-uint32_t targetTime = 0;       // for next 1 second timeout
-
-byte omm = 99;
 boolean initial = 1;
 byte xcolon = 0;
 unsigned int colour = 0;
 
-static uint8_t conv2d(const char* p) {
-  uint8_t v = 0;
-  if ('0' <= *p && *p <= '9')
-    v = *p - '0';
-  return 10 * v + *++p - '0';
-}
-
-uint8_t hh=conv2d(__TIME__), mm=conv2d(__TIME__+3), ss=conv2d(__TIME__+6);  // Get H, M, S from compile time
-
-//void displaysetup(void);
-
 void displaySetup(void){
 
 	tft.init();
-	tft.setRotation(1);
+	tft.setRotation(0);
 	tft.fillScreen(TFT_BLACK);
 
 	tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Note: the new fonts do not draw the background colour
-
-	targetTime = millis() + 1000; 
 }
 
 // SERVO ================================================================
@@ -177,7 +140,7 @@ void ultrasonicSetup(void)
 
 TaskHandle_t xTask1Handle,xTask2Handle, xTask3Handle, xTask4Handle;
 TaskHandle_t xTask5Handle,xTask6Handle, xTask7Handle, xTask8Handle;
-TaskHandle_t xTask9Handle; //,xTask6Handle, xTask7Handle, xTask8Handle;
+//TaskHandle_t xTask9Handle; //,xTask6Handle, xTask7Handle, xTask8Handle;
 
 
 void vTask1(void *arg);
@@ -262,74 +225,6 @@ void vTask7(void *arg) // Display
 
 	while(1)
 	{
-
-    if (targetTime < millis()) 
-    {
-      targetTime = millis() + 1000;
-      ss++; // Advance second
-
-      if (ss == 60) 
-      {
-        ss = 0;
-        omm = mm;
-        mm++; // Advance minute
-
-        if(mm > 59) 
-        {
-          mm = 0;
-          hh++; // Advance hour
-
-          if (hh > 23) 
-          {
-            hh = 0;
-          }
-        }
-      }      
-
-      if (ss == 0 || initial) 
-      {
-        initial = 0;
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.setCursor (50, 0);
-        tft.print(__DATE__); // This uses the standard ADAFruit small font
-
-        //tft.setTextColor(TFT_BLUE, TFT_BLACK);
-        //tft.drawCentreString("It is windy", 120, 48, 2); // Next size up font 2
-
-        //tft.setTextColor(0xF81F, TFT_BLACK); // Pink
-        //tft.drawCentreString("12.34",80,100,6); // Large font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 . : a p m
-      }
-      // Update digital time
-      byte xpos = 0;
-      byte ypos = 0;
-
-      if (omm != mm) 
-      { // Only redraw every minute to minimise flicker
-        // Uncomment ONE of the next 2 lines, using the ghost image demonstrates text overlay as time is drawn over it
-        tft.setTextColor(0x39C4, TFT_BLACK);  // Leave a 7 segment ghost image, comment out next line!
-        //tft.setTextColor(TFT_BLACK, TFT_BLACK); // Set font colour to black to wipe image
-        // Font 7 is to show a pseudo 7 segment display.
-        // Font 7 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 0 : .
-        tft.drawString("88:88", xpos, ypos, 2); // Overwrite the text to clear it
-        tft.setTextColor(0xFBE0, TFT_BLACK); // Orange
-        omm = mm;
-
-        if (hh < 10) xpos+= tft.drawChar('0', xpos, ypos, 2);
-        xpos+= tft.drawNumber(hh, xpos, ypos, 2);
-        xcolon = xpos;
-        xpos+= tft.drawChar(':', xpos, ypos, 2);
-        if (mm < 10) xpos+= tft.drawChar('0', xpos, ypos, 2);
-        tft.drawNumber(mm, xpos, ypos, 2);
-      }
-
-      else if (ss%2) 
-      { // Flash the colon
-        tft.setTextColor(0x39C4, TFT_BLACK);
-        xpos+= tft.drawChar(':', xcolon, ypos, 2);
-        tft.setTextColor(0xFBE0, TFT_BLACK);
-      }
-    }
-    
     // IMPRIME DISTANCIA EM cm NO DISPLAY TFT ==================================
     float updatecm;
 
@@ -395,7 +290,6 @@ void setup()
 	xTaskCreate(vTask7, "Display", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask7Handle);
   xTaskCreate(vTask8, "Ultrasonic", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask8Handle);
   //xTaskCreate(vTask9, "MQTT", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask9Handle);
-
 }
 
 // MQTT
