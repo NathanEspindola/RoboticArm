22c2330573e6e6e340c3541245e844da55056358
NathanEspindola <espindolathan@gmail.com>
2023-05-27 23:46:31 -0300

    27/05

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,9 @@
  * Trabalho de Conclusão de Curso
  * Aluno Nathan Espindola
  * Orientador Professor Mestre Julio Cesar Marques de Lima
- * This sketch uses the Arduino ESP32 Servo Library to sweep 4 servos in sequence.
- * 
- * Different servos require different pulse widths to vary servo angle, but the range is 
- * an approximately 500-2500 microsecond pulse every 20ms (50Hz). In general, hobbyist servos
- * sweep 180 degrees, so the lowest number in the published range for a particular servo
- * represents an angle of 0 degrees, the middle of the range represents 90 degrees, and the top
- * of the range represents 180 degrees. So for example, if the range is 1000us to 2000us,
- * 1000us would equal an angle of 0, 1500us would equal 90 degrees, and 2000us would equal 1800
- * degrees.
- * 
- * Circuit:
- * Servo motors have three wires: power, ground, and signal. The power wire is typically red,
- * the ground wire is typically black or brown, and the signal wire is typically yellow,
- * orange or white. Since the ESP32 can supply limited current at only 3.3V, and servos draw
- * considerable power, we will connect servo power to the VBat pin of the ESP32 (located
- * near the USB connector). THIS IS ONLY APPROPRIATE FOR SMALL SERVOS. 
- * 
- * We could also connect servo power to a separate external
- * power source (as long as we connect all of the grounds (ESP32, servo, and external power).
- * In this example, we just connect ESP32 ground to servo ground. The servo signal pins
- * connect to any available GPIO pins on the ESP32 (in this example, we use pins
- * 22, 19, 23, & 18).
- * 
- * In this example, we assume four Tower Pro SG90 small servos.
- * The published min and max for this servo are 500 and 2400, respectively.
- * These values actually drive the servos a little past 0 and 180, so
- * if you are particular, adjust the min and max values to match your needs.
- * Experimentally, 550 and 2350 are pretty close to 0 and 180.
  */
 
-// PIN MANAGER
+// P I N   M A N A G E R
 // 12 - Servo1
 // 13 - Servo2
 // 15 - Servo3
@@ -53,7 +25,6 @@
 // 37
 // SVP
 
-//#include <Arduino.h>
 #include <ESP32Servo.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
@@ -63,7 +34,7 @@
 
 // DISPLAY ================================================================
 
-TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+TFT_eSPI tft = TFT_eSPI(); 
 
 boolean initial = 1;
 byte xcolon = 0;
@@ -75,7 +46,7 @@ void displaySetup(void){
 	tft.setRotation(0);
 	tft.fillScreen(TFT_BLACK);
 
-	tft.setTextColor(TFT_YELLOW, TFT_BLACK); // Note: the new fonts do not draw the background colour
+	tft.setTextColor(TFT_YELLOW, TFT_BLACK); 
 }
 
 // SERVO ================================================================
@@ -120,6 +91,13 @@ void vInitHW(void)
 	servo5.setPeriodHertz(250);    
 	servo6.setPeriodHertz(250);    
 
+	servo1.attach(servo1Pin, minUs1, maxUs1);
+	servo2.attach(servo2Pin, minUs1, maxUs1);
+	servo3.attach(servo3Pin, minUs1, maxUs1);
+	servo4.attach(servo4Pin, minUs2, maxUs2);
+	servo5.attach(servo5Pin, minUs2, maxUs2);
+	servo6.attach(servo6Pin, minUs2, maxUs2);
+
 }
 
 // ULTRASONIC ================================================================
@@ -157,7 +135,7 @@ void vTask8(void *arg);
 
 
 void loop() { 
-    vTaskDelay(pdMS_TO_TICKS(3000));    /* Delay de 3 segundos */
+    vTaskDelay(pdMS_TO_TICKS(3000));
 }
 
 void vTask1(void *arg) // Controle Servo 1
@@ -166,27 +144,20 @@ void vTask1(void *arg) // Controle Servo 1
 	
 	while(1)
 	{
-		servo1.attach(servo1Pin, minUs1, maxUs1);
-		pwm.attachPin(27, 10000);//10khz
 
-    
-
-      if (distance <= 10)
+    if (distance <= 10)
       {
-        servo1.write(0);
-        delay(20);             // waits 20ms for the servo to reach the position
+        servo1.write(45);
+        delay(20);             
       }
 
-      if (distance > 10) 
+    if (distance > 10) 
       {
         servo1.write(90);
         delay(20);
       }		
 
-		servo1.detach();
-		pwm.detachPin(27);
-
-    vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+    vTaskDelay(pdMS_TO_TICKS(100));    
 
 	}
 }
@@ -197,151 +168,112 @@ void vTask2(void *arg) // Controle Servo 2
 
 	while(1)
 	{
+		if (distance <= 10)
+      	{
+        	servo2.write(45);
+        	delay(20);           
+      	}
 
-		servo2.attach(servo2Pin, minUs1, maxUs1);
-		pwm.attachPin(27, 10000);//10khz
+    	if (distance > 10) 
+      	{
+        	servo2.write(90);
+        	delay(20);
+      	}	
 
-/*		for (int pos2 = 0; pos2 <= 180; pos2 += 1) { // sweep from 0 degrees to 180 degrees
-			servo2.write(pos2);
-			delay(20);             // waits 20ms for the servo to reach the position
-		}
-
-		for (int pos2 = 180; pos2 >= 0; pos2 -= 1) { // sweep from 180 degrees to 0 degrees
-			servo2.write(pos2);
-			delay(20);
-		}		
-*/
-		servo2.write(0);
-			delay(20);             // waits 20ms for the servo to reach the position
-
-		servo2.detach();
-		pwm.detachPin(27);
-
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));    
 
 	}
 }
 
-void vTask3(void *arg) // Controle Servo 2
+void vTask3(void *arg) // Controle Servo 3
 {
 	(void) arg;
 
 	while(1)
 	{
 
-		servo3.attach(servo3Pin, minUs1, maxUs1);
-		pwm.attachPin(27, 10000);//10khz
+      if (distance <= 10)
+      {
+        servo3.write(45);
+        delay(20);          
+      }
 
-/*		for (int pos2 = 0; pos2 <= 180; pos2 += 1) { // sweep from 0 degrees to 180 degrees
-			servo2.write(pos2);
-			delay(20);             // waits 20ms for the servo to reach the position
-		}
+      if (distance > 10) 
+      {
+        servo3.write(90);
+        delay(20);
+      }	
 
-		for (int pos2 = 180; pos2 >= 0; pos2 -= 1) { // sweep from 180 degrees to 0 degrees
-			servo2.write(pos2);
-			delay(20);
-		}		
-*/
-		servo3.write(0);
-			delay(20);             // waits 20ms for the servo to reach the position
-
-		servo3.detach();
-		pwm.detachPin(27);
-
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));    
 
 	}
 }
 
-void vTask4(void *arg) // Controle Servo 2
+void vTask4(void *arg) // Controle Servo 4
 {
 	(void) arg;
 
 	while(1)
 	{
+		if (distance <= 10)
+      {
+        servo4.write(90);
+        delay(20);      
+      }
 
-		servo4.attach(servo4Pin, minUs2, maxUs2);
-		pwm.attachPin(27, 10000);//10khz
+      if (distance > 10) 
+      {
+        servo4.write(135);
+        delay(20);
+      }	
 
-/*		for (int pos2 = 0; pos2 <= 180; pos2 += 1) { // sweep from 0 degrees to 180 degrees
-			servo2.write(pos2);
-			delay(20);             // waits 20ms for the servo to reach the position
-		}
-
-		for (int pos2 = 180; pos2 >= 0; pos2 -= 1) { // sweep from 180 degrees to 0 degrees
-			servo2.write(pos2);
-			delay(20);
-		}		
-*/
-		servo4.write(0);
-			delay(20);             // waits 20ms for the servo to reach the position
-
-		servo4.detach();
-		pwm.detachPin(27);
-
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));   
 
 	}
 }
 
-void vTask5(void *arg) // Controle Servo 2
+void vTask5(void *arg) // Controle Servo 5
 {
 	(void) arg;
 
 	while(1)
 	{
+      if (distance <= 10)
+      {
+        servo5.write(45);
+        delay(20);    
+      }
 
-		servo5.attach(servo5Pin, minUs2, maxUs2);
-		pwm.attachPin(27, 10000);//10khz
+      if (distance > 10) 
+      {
+        servo5.write(90);
+        delay(20);
+      }	
 
-/*		for (int pos2 = 0; pos2 <= 180; pos2 += 1) { // sweep from 0 degrees to 180 degrees
-			servo2.write(pos2);
-			delay(20);             // waits 20ms for the servo to reach the position
-		}
-
-		for (int pos2 = 180; pos2 >= 0; pos2 -= 1) { // sweep from 180 degrees to 0 degrees
-			servo2.write(pos2);
-			delay(20);
-		}		
-*/
-		servo5.write(0);
-			delay(20);             // waits 20ms for the servo to reach the position
-
-		servo5.detach();
-		pwm.detachPin(27);
-
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));    
 
 	}
 }
 
-void vTask6(void *arg) // Controle Servo 2
+void vTask6(void *arg) // Controle Servo 6
 {
 	(void) arg;
 
 	while(1)
 	{
+		      if (distance <= 10)
+      {
+        servo6.write(135);
+        delay(20);            
+      }
 
-		servo6.attach(servo6Pin, minUs2, maxUs2);
-		pwm.attachPin(27, 10000);//10khz
+      if (distance > 10) 
+      {
+        servo6.write(90);
+        delay(20);
+      }	
 
-/*		for (int pos2 = 0; pos2 <= 180; pos2 += 1) { // sweep from 0 degrees to 180 degrees
-			servo2.write(pos2);
-			delay(20);             // waits 20ms for the servo to reach the position
-		}
-
-		for (int pos2 = 180; pos2 >= 0; pos2 -= 1) { // sweep from 180 degrees to 0 degrees
-			servo2.write(pos2);
-			delay(20);
-		}		
-*/
-		servo6.write(0);
-			delay(20);             // waits 20ms for the servo to reach the position
-
-		servo6.detach();
-		pwm.detachPin(27);
-
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));  
 
 	}
 }
@@ -365,7 +297,7 @@ void vTask7(void *arg) // Display
     tft.drawNumber(distance, 0, 75, 7);
     updatecm = distance;
 
-    vTaskDelay(pdMS_TO_TICKS(10));     /* Delay de 1 segundos */
+    vTaskDelay(pdMS_TO_TICKS(10));     
   }
 }
 
@@ -395,7 +327,7 @@ void vTask8(void *arg) // Ultrasonic
     Serial.print("Distance (cm): ");
     Serial.println(distanceCm);
 
-   	vTaskDelay(pdMS_TO_TICKS(100));     /* Delay de 1 segundos */
+   	vTaskDelay(pdMS_TO_TICKS(100));   
 	}
 }
 
@@ -408,19 +340,13 @@ void setup()
 	ultrasonicSetup();
 
 	// Endereço da Task1, Identificação da Task, Bytes Disponibilizados para Task1, Permite Enviar um Parametro para Task, Prioridade (pode ser de 1 a 5), Handler
-	xTaskCreate(vTask1, "servo1", configMINIMAL_STACK_SIZE + 1048, NULL, 2, &xTask1Handle);
-	xTaskCreate(vTask2, "servo2", configMINIMAL_STACK_SIZE + 1048, NULL, 2, &xTask2Handle);
-    xTaskCreate(vTask3, "servo3", configMINIMAL_STACK_SIZE + 1048, NULL, 2, &xTask3Handle);
+	xTaskCreate(vTask1, "servo1", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask1Handle);
+	xTaskCreate(vTask2, "servo2", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask2Handle);
+    xTaskCreate(vTask3, "servo3", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask3Handle);
 	xTaskCreate(vTask4, "servo4", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask4Handle);
 	xTaskCreate(vTask5, "servo5", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask5Handle);
 	xTaskCreate(vTask6, "servo6", configMINIMAL_STACK_SIZE + 1048, NULL, 3, &xTask6Handle);
-	xTaskCreate(vTask7, "Display", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask7Handle);
-  	xTaskCreate(vTask8, "Ultrasonic", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask8Handle);
+	xTaskCreate(vTask7, "Display", configMINIMAL_STACK_SIZE + 1048, NULL, 2, &xTask7Handle);
+  	xTaskCreate(vTask8, "Ultrasonic", configMINIMAL_STACK_SIZE + 1048, NULL, 2, &xTask8Handle);
   //xTaskCreate(vTask9, "MQTT", configMINIMAL_STACK_SIZE + 1048, NULL, 1, &xTask9Handle);
 }
-
-// MQTT
-// Tela TFT Interativa
-// Camera com Reconhecimento
-// Rodas?
-// 
